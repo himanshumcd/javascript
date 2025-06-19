5cd4fa34ea67cabb6f935606cb49226819fd9577
himanshumcd <150276010+himanshumcd@users.noreply.github.com>
2025-06-19 17:48:16 +0000

    basic stuff

---
+++ colorchanger/bgchanger/src/changer.jsx
@@ -0,0 +1,19 @@
+
+function Changer(){
+    return(
+        <>
+        <div className="bg-white ">
+      <button>red </button>  
+      <button>blue </button>  
+      <button>pink </button>  
+      <button>green</button>  
+      <button>orange</button>  
+      <button>skyBlue</button>  
+        
+        </div>
+        
+        
+        </>
+    )
+}
+export default Changer
